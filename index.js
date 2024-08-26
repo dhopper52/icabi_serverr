c15301563ab69aa90870a4ec0a198dbc615b5a8e
dhopper52 <dhopper.tayyab@gmail.com>
2024-08-26 21:58:21 +0500

    ddd

--- index.js
+++ index.js
@@ -10,12 +10,7 @@ app.use(bodyParser.json());
 
 app.get("/api/bookings", async (req, res) => {
   const { auth, startDate, endDate, offset } = req.query;
-  console.log(req.query, "bookings queryyyyyyyyyyyyyyy");
-  console.log({ auth }, { startDate }, { endDate }, { offset });
   const url = `https://api.icabbicanada.com/ca2/bookings/history/?order=closed_date&from=${startDate}T00:30:00&to=${endDate}T23:30:00&Order=id&direction=DESC&status=COMPLETED&offset=${offset}&limit=50`;
-  // const url = `https://api.icabbicanada.com/ca2/bookings/history/?order=closed_date&from=2024-08-21T00:30:00&to=2024-08-21T23:30:00&Order=id&direction=DESC&status=COMPLETED&offset=500&limit=499`;
-// const url = "https://api.icabbicanada.com/ca2/bookings/history/?order=closed_date&from=2023-08-22T00:30:00&to=2024-08-22T23:30:00&Order=id&direction=DESC&status=COMPLETED&offset=500&limit=499"
-  console.log(url, "bookings urlllllllllllllllllllllllllllllll");
 
   try {
     const response = await axios.get(url, {
@@ -35,12 +30,10 @@ app.get("/api/bookings", async (req, res) => {
 
 app.get("/api/driver", async (req, res) => {
   const { auth, startDate, endDate } = req.query;
-  console.log(req.query, "driver queryyyyyyyyyyyyyyy");
-  console.log({ auth }, { startDate }, { endDate });
+
 
   const url = `https://api.icabbicanada.com/ca2/drivers/hours/?from=${startDate}T00:30:00&to=${endDate}T23:30:00`;
-  // const url = `https://api.icabbicanada.com/ca2/drivers/hours/?from=2024-08-21T00:30:00&to=2024-08-21T23:30:00`;
-  console.log(url, "driver urlllllllllllllllllllllllllllllll");
+ 
 
   try {
     const response = await axios.get(url, {
